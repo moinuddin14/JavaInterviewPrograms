f4c84748d0a1e852cff99d2cd597c22dc8c3b872
Khaja <moinuddinbtech@gmail.com>
2018-06-04 05:29:04 +0530

    Added PrintDiamondBorder Program

--- src/test/java/com/java/programs/PrintDiamondBorder.java
+++ src/test/java/com/java/programs/PrintDiamondBorder.java
@@ -1,11 +1,11 @@
 package com.java.programs;
 
-public class PrintPyramid {
+public class PrintDiamondBorder {
     public static void main(String[] args) {
-        printPyramid(10);
+        printDiamondBorder(7);
     }
 
-    public static void printPyramid(int evenNumber) {
+    public static void printDiamondBorder(int evenNumber) {
         printSpaces(evenNumber+1);
         System.out.println("*");
 
@@ -28,10 +28,27 @@ public class PrintPyramid {
             System.out.println("");
         }
 
-        for(int i = 0;i <= (evenNumber+1); i++) {
-            System.out.print(" *");
+        for(int i = 0; i <=evenNumber; i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for(int k= i;k >= i;k--) {
+                System.out.print("*");
+            }
+            for(int l = evenNumber-1;l >= i;l--) {
+                System.out.print(" ");
+            }
+            for(int l = evenNumber;l >= i;l--) {
+                System.out.print(" ");
+            }
+            for(int k= i;k >= i;k--) {
+                System.out.print("*");
+            }
+            System.out.println("");
         }
 
+        printSpaces(evenNumber+1);
+        System.out.println("*");
     }
 
     public static void printSpaces(int num) {
