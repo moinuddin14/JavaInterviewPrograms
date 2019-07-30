6c40b21e96949d4f9f53502ddad869d69559e4cd
Khaja Moinuddin Mohammed <moinuddinbtech@gmail.com>
2019-07-30 10:50:22 +0530

    Create ReplaceWithAlphabetPositon.js

---
+++ src/test/java/com/java/programs/ReplaceWithAlphabetPositon.js
@@ -0,0 +1,15 @@
+function alphabetPosition(text) {
+// Map to hold the values of each character in alphabet
+  let map = new Map([['a', 1], ['b', 2], ['c',3], ['d',4], ['e',5], ['f',6], ['g',7], ['h',8], ['i',9], ['j',10], ['k',11], ['l',12], ['m',13], ['n',14], ['o',15], ['p',16], ['q',17], ['r',18], ['s',19], ['t',20], ['u',21], ['v',22], ['w',23], ['x',24], ['y',25], ['z',26],
+                    ['A', 1], ['B', 2], ['C',3], ['D',4], ['E',5], ['F',6], ['G',7], ['H',8], ['I',9], ['J',10], ['K',11], ['L',12], ['M',13], ['N',14], ['O',15], ['P',16], ['Q',17], ['R',18], ['S',19], ['T',20], ['U',21], ['V',22], ['W',23], ['X',24], ['Y',25], ['Z',26]]);
+
+  const modifiedText = text.split("").map((val, key) => {
+    if(map.has(val)) {
+      return map.get(val)+" ";
+    }
+    else {
+    }
+  });
+  
+  return modifiedText.join("").trim();
+}
